59c7eeda5da3f3240d695b70c1338fb61bcf8794
kirbyclopez <kirbyclopez@gmail.com>
2023-01-19 14:09:16 +0800

    build: updated build

--- build/utils/cookie-config.js
+++ build/utils/cookie-config.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const cookieConfig = {
     httpOnly: true,
+    secure: process.env.COOKIE_SECURE === "true",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : false,
     maxAge: 60 * 60 * 24 * 30,
     path: "/",
 };
